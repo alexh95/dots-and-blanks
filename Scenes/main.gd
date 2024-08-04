d0c8a911c4e9d2751557dd34a3db78b2d975ede2
alexh95 <alexhuruiala@gmail.com>
2024-08-04 03:47:14 +0300

    feat: implemented validity check for placing pieces
    Can now only place domino pieces inside the grid and not intersecting other pieces.

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -11,7 +11,10 @@ func randomize_dots():
 func _ready():
 	randomize_dots()
 	var newDominoPiece = dominoGenerator.instantiate()
-	calculateDominoGhostPosition(Vector2i(2, 2))
+	hoverGridPosition = Vector2i(2, 2)
+	calculateDominoGhostPosition(hoverGridPosition)
+	newDominoPiece.vertical = dominoVertical
+	newDominoPiece.gridPosition = hoverGridPosition
 	newDominoPiece.position = $DominoContainer/DominoGhost.position
 	newDominoPiece.putDots($DominoContainer/DominoGhost.dotsTop, $DominoContainer/DominoGhost.dotsBot);
 	$DominoContainer/Dominoes.add_child(newDominoPiece)
@@ -21,25 +24,32 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		calculateDominoGhostPositionFromScreen(event.position)
 	if event is InputEventMouseButton:
-		calculateDominoGhostPositionFromScreen(event.position)
+		var canBePlaced = calculateDominoGhostPositionFromScreen(event.position)
 		if (event.button_index == 1 and event.button_mask > 0):
-			var newDominoPiece = dominoGenerator.instantiate()
-			newDominoPiece.position = $DominoContainer/DominoGhost.position
-			newDominoPiece.rotation = $DominoContainer/DominoGhost.rotation
-			newDominoPiece.putDots($DominoContainer/DominoGhost.dotsTop, $DominoContainer/DominoGhost.dotsBot);
-			$DominoContainer/Dominoes.add_child(newDominoPiece)
-			randomize_dots()
+			if canBePlaced:
+				var newDominoPiece = dominoGenerator.instantiate()
+				newDominoPiece.vertical = dominoVertical
+				newDominoPiece.gridPosition = hoverGridPosition
+				newDominoPiece.position = $DominoContainer/DominoGhost.position
+				newDominoPiece.rotation = $DominoContainer/DominoGhost.rotation
+				newDominoPiece.putDots($DominoContainer/DominoGhost.dotsTop, $DominoContainer/DominoGhost.dotsBot);
+				$DominoContainer/Dominoes.add_child(newDominoPiece)
+				randomize_dots()
 		if (event.button_index == 2 and event.button_mask > 0):
 			dominoVertical = !dominoVertical
 			$DominoContainer/DominoGhost.rotation += 0.5 * PI
 			calculateDominoGhostPositionFromScreen(event.position)
 			
 	if event is InputEventKey:
-		if event.pressed && !event.echo && event.keycode == 70:
+		if event.keycode == 70 && event.pressed && !event.echo:
 			if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
 				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 			elif DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		if event.keycode == 82 && event.pressed && !event.echo:
+			randomize_dots()
+		if event.keycode == 77 && event.pressed && !event.echo:
+			print('m')
 
 func closestDomino(position: Vector2):
 	var result = $DominoContainer/Dominoes.get_children(true)[0]
@@ -53,7 +63,15 @@ func closestDomino(position: Vector2):
 	
 func calculateDominoGhostPositionFromScreen(screenPosition):
 	hoverGridPosition = $DominoContainer/DominoGrid.getClosestGridPosition(screenPosition)
+	var isInGrid = $DominoContainer/DominoGrid.isInsideGrid(hoverGridPosition, dominoVertical)
+	var isNotColliding = checkCollisions(hoverGridPosition, dominoVertical)
+	var validGridPosition = isInGrid && isNotColliding
+	if validGridPosition:
+		$DominoContainer/DominoGhost.modulate = Color(0.0, 1.0, 0.0, 0.5)
+	else:
+		$DominoContainer/DominoGhost.modulate = Color(1.0, 0.0, 0.0, 0.5)
 	calculateDominoGhostPosition(hoverGridPosition)
+	return validGridPosition
 	
 func calculateDominoGhostPosition(gridPosition):
 	var dominoScreenPosition = $DominoContainer/DominoGrid.getGridScreenPosition(gridPosition)
@@ -62,3 +80,11 @@ func calculateDominoGhostPosition(gridPosition):
 	else:
 		dominoScreenPosition += $DominoContainer/DominoGrid.horizontalCellOffset
 	$DominoContainer/DominoGhost.position = dominoScreenPosition
+	
+func checkCollisions(gridPosition: Vector2i, vertical: bool):
+	for domino in $DominoContainer/Dominoes.get_children(true):
+		var dominoSecondGridPosition = Vector2i(domino.gridPosition.x, domino.gridPosition.y + 1) if domino.vertical else Vector2i(domino.gridPosition.x + 1, domino.gridPosition.y)
+		var secondGridPosition = Vector2i(gridPosition.x, gridPosition.y + 1) if vertical else Vector2i(gridPosition.x + 1, gridPosition.y)
+		if (domino.gridPosition == gridPosition) or (domino.gridPosition == secondGridPosition) or (dominoSecondGridPosition == gridPosition) or (dominoSecondGridPosition == secondGridPosition):
+			return false
+	return true
