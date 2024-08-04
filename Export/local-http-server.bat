3b3751195cde2677fefe257786415a7d8d3ef39f
alexh95 <alexhuruiala@gmail.com>
2024-08-04 11:40:26 +0300

    chore: updated export options
    Windows:
    - embedded the pck into the exe
    - exluded the console wrapper
    
    Web:
    - enabled PWA

---
+++ Export/local-http-server.bat
@@ -0,0 +1,3 @@
+REM Using local web server: https://github.com/lwsjs/local-web-server
+REM Run this to test the HTML5 export.
+bunx local-web-server --directory Web --port 8080 --cors.opener-policy same-origin --cors.embedder-policy require-corp
